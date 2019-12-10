1bb01b2c026d2aef0354c9228e0c2c972b681a14
jacob2148 <jtd145@scarletmail.rutgers.edu>
2019-12-10 10:52:37 -0500

    added background and moved point function
    background added along with moving point function below translate function

--- sketch.js
+++ sketch.js
@@ -9,23 +9,26 @@ function setup() {
 var move = 0
 var currentX = 0;
 var currentY = 0;
-function draw() {
+function draw()
+background(325);
+fill(0);{
   if(keyIsPressed === true){
     //anytime a key is pressed should fill in the points in various locations
     fill(0);
   }else{
     fill(255);
   }
-  point (100, 100)
+
   //added a physical object on screen to see the changes
   strokeWeight(15)
   //added to the width of the dot as to be seen by the user
   translate(move, 40, 40)
+  point (100, 100)
   //causes a new spot to form on the canvas when the the next key is pressed with a change vertically and horizontally
   currentX = (currentX + 25);
   currentY = (currentY + 25);
   move++
-  if(move == 400, 400){move -= 550, 550;}}
+  if(move == 400, 0){move -= 550, 550;}}
   //resets the canvas to allow for the points to loop back around on to the visible area
 
   // put drawing code here
